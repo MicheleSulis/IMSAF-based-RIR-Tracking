40a61c2bce2417b51498b066be1306f6a18fa09e
Mauro Costantino <mauro.costantin02@gmail.com>
2026-07-12 11:41:15 +0200

    Fix commenti buffer doppio

--- C/Template/Template/PlugIn.cpp
+++ C/Template/Template/PlugIn.cpp
@@ -173,12 +173,12 @@ int __stdcall PlugIn::LEPlugin_Process(PinType** Input, PinType** Output, LPVOID
 
 				// Nota sul doppio buffer: se usassimo un buffer circolare classico, supponendo di avere 5 elementi, potremmo essere in una situazione,
 				// rispetto ad un istante x_i, del tipo (ipotizzando head = 1):
-				// [x_{i-1}, x_{i}, x_{i-4}, x_{i-3}, x_{i-2}]
+				// [x_{i-4}, x_{i}, x_{i-1}, x_{i-2}, x_{i-3}]
 				// In tal caso, poiché avremmo bisogno dei campioni fino a x_{i-4}, dovremmo fare due chiamate a ippsDotProd o rielaborare il buffer.
 
 				// Al contrario, scrivendo i campioni due volte separati dalla lunghezza del filtro, otterremmo, nello stesso scenario, un vettore nella forma:
-				// [x_{i-1}, x_{i}, x_{i-4}, x_{i-3}, x_{i-2} | x_{i-1}, x_{i}, x_{i-4}, x_{i-3}, x_{i-2}]
-				// In questo caso, usando ippsDotProd partendo da x_{i} (head = 1) e leggendo 5 campioni, otterremmo proprio [x_{i}, x_{i-4}, x_{i-3}, x_{i-2} | x_{i-1}]
+				// [x_{i-4}, x_{i}, x_{i-1}, x_{i-2}, x_{i-3} | x_{i-4}, x_{i}, x_{i-1}, x_{i-2}, x_{i-3}]
+				// In questo caso, usando ippsDotProd partendo da x_{i} (head = 1) e leggendo 5 campioni, otterremmo proprio [x_{i}, x_{i-1}, x_{i-2}, x_{i-3} | x_{i-4}]
 				ippsDotProd_64f64fc(taps, state_ptr, filter_len, &filter_out);
 
 				Ipp64fc rotor_syn = dft_rot_syn[i * I + n_mod];
